bea9867fd58afa2e733689390b46827030bffe12
Nicolás Rafael Riquelme Guzmán <nrriquelme@gmail.com>
2017-02-23 18:17:08 -0300

    Añadidas rutas

--- angular-cliente/src/app/app.module.ts
+++ angular-cliente/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+// modulo para establecer las rutas.
+import { RouterModule, Routes } from '@angular/router';
+// flash messages
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +13,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+// estableciendo las rutas para cada componente de la pagina.
+const appRutas: Routes = [
+  // primer elemento es la pagina de inicio.
+  { path: '', component: HomeComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +35,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRutas),
+    FlashMessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
