7fc582a9e9c6ee308ab3303b7a06f6a79618d405
Nicolás Rafael Riquelme Guzmán <nrriquelme@gmail.com>
2017-02-22 23:57:40 -0300

    Componentes de UI

--- angular-cliente/src/app/app.module.ts
+++ angular-cliente/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
