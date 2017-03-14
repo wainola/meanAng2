ba688a42516aa23096fdbf7ac56a41e81a4cd40d
Nicolás Rafael Riquelme Guzmán <nrriquelme@gmail.com>
2017-03-14 11:39:01 -0300

    linkeada a la base de datos

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // ruta hacia la base de datos en Mongo
-    database: 'mongodb://localhost:27017/prototipo',
+    database: 'mongodb://nrriquel:Nrriquel1987@dbh84.mlab.com:27847/cms_express_ang',
     secret: 'misecreto'
 }
